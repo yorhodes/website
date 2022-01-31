d4d66d2bd580dddffd435c9e3a21c59b7311bbb2
yorhodes <yorkerhodesiv@gmail.com>
2022-01-30 18:19:45 -0800

    Add frameworks

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // from https://mdxjs.com/docs/getting-started/#nextjs
+  experimental: {esmExternals: true},
+  // Support MDX files as pages:
+  pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
+  // Support loading `.md`, `.mdx`:
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.mdx?$/,
+      use: [
+        // The default `babel-loader` used by Next:
+        options.defaultLoaders.babel,
+        {
+          loader: '@mdx-js/loader',
+          /** @type {import('@mdx-js/loader').Options} */
+          options: {/* jsxImportSource: …, otherOptions… */}
+        }
+      ]
+    })
+
+    return config
+  }
 }
 
 module.exports = nextConfig
