a11237de6d0f10dae3cf2add71b8ea71b42aa84f
yorhodes <yorkerhodesiv@gmail.com>
2022-02-23 12:09:03 -0800

    Add browser title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import theme from "../theme"
 const App = ( {Component, pageProps} : AppProps) => {
   return (
     <ChakraProvider theme={theme}>
+      <title>Yorke Rhodes - Developer</title>
       <Component {...pageProps} />
     </ChakraProvider>
   )
