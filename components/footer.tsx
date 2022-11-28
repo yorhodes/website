ea18d2441b27f494fc86729b5ecbf82c8ce4b1f7
Yorke Rhodes <email@yorke.dev>
2022-11-27 19:03:33 -0500

    Load vercel env in footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,32 +5,41 @@ import { BiGitRepoForked } from "react-icons/bi";
 import { FaEdit, FaStar } from "react-icons/fa";
 import { LogoLink } from "./link";
 
+const getRepo = () => {
+  // https://vercel.com/docs/concepts/projects/environment-variables#system-environment-variables
+  const platform = process.env.VERCEL_GIT_PROVIDER ?? 'github';
+  const name = process.env.VERCEL_GIT_REPO_SLUG ?? "website"
+  const owner = process.env.VERCEL_GIT_REPO_OWNER ?? "yorhodes";
+  const branch = process.env.VERCEL_GIT_COMMIT_REF ?? 'main';
 
-// https://vercel.com/docs/concepts/projects/environment-variables#system-environment-variables
-const GIT_PLATFORM = process.env.VERCEL_GIT_PROVIDER ?? 'github';
-const GIT_REPO = process.env.VERCEL_GIT_REPO_SLUG ?? "website"
-const GIT_OWNER = process.env.VERCEL_GIT_REPO_OWNER ?? "yorhodes";
-const GIT_BRANCH = process.env.VERCEL_GIT_COMMIT_REF ?? 'main';
+  const repo = `https://${platform}.com/${owner}/${name}`;
 
-const REPO_URL = `https://${GIT_PLATFORM}.com/${GIT_OWNER}/${GIT_REPO}`;
+  return {
+    repo,
+    branch,
+  };
+}
 
 const Footer = () => {
+
   const router = useRouter();
   const file = router.pathname === "/" ? "bio" : router.pathname.slice(1);
 
+  const { repo, branch } = getRepo();
+
   return (<HStack paddingY="5" alignSelf="right">
     {LogoLink({
-      link: `${REPO_URL}/generate`,
+      link: `${repo}/generate`,
       icon: BiGitRepoForked,
       label: "use",
     })}
     {LogoLink({
-      link: `${REPO_URL}/edit/${GIT_BRANCH}/data/${file}.json`,
+      link: `${repo}/edit/${branch}/data/${file}.json`,
       icon: FaEdit,
       label: "edit"
     })}
     {LogoLink({
-      link: REPO_URL,
+      link: repo,
       icon: FaStar,
       label: "star"
     })}
