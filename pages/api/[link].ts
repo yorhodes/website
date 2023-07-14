029cbe16e01d69307444dffb992859f848de556a
Yorke Rhodes <email@yorke.dev>
2023-07-14 11:09:58 -0400

    Add writing links to link API

--- pages/api/[link].ts
+++ pages/api/[link].ts
@@ -1,11 +1,17 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
-import links from '../../data/links.json';
+import links from "../../data/links.json";
+import writing from "../../data/writing.json";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { link } = req.query;
 
-  const result = links.find((o) => o.link.includes(link.toString()) || o.label?.includes(link.toString()));
+  const result = [...links, ...writing].find(
+    (o) =>
+      o.link.includes(link.toString()) || 
+      'label' in o && o.label?.includes(link.toString()) ||
+      'title' in o && o.title?.includes(link.toString())
+  );
 
   if (result) {
     res.status(200).redirect(result.link);
