1470cc966a18aead00c9a1317b2137b98b7a933f
Yorke Rhodes <email@yorke.dev>
2023-07-18 23:20:33 -0400

    Small tweaks

--- components/appendix.tsx
+++ components/appendix.tsx
@@ -2,12 +2,13 @@ import { Link, useColorModeValue, useHighlight } from "@chakra-ui/react";
 
 const Appendix = (text: string, appendix: Record<string, string>) => {
   const chunks = useHighlight({ text, query: Object.keys(appendix) });
-  const color = useColorModeValue("gray.100", "gray.600");
+  const color = useColorModeValue("gray.100", "gray.700");
   return chunks.map(({ match, text }) => {
     if (!match) return text;
     return (
       <Link
-        rounded="full"
+        rounded="md"
+        padding="0.1rem"
         background={color}
         key={text}
         href={appendix[text]}
