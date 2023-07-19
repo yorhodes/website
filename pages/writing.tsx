b56ed9fe5d9a22a52b2ca8d1672b1aa6d24d91ac
Yorke Rhodes <email@yorke.dev>
2023-07-18 21:03:06 -0400

    Fix build

--- pages/writing.tsx
+++ pages/writing.tsx
@@ -1,15 +1,15 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Container, Flex } from "@chakra-ui/react";
 
 import WritingElem from "../components/writing";
+
 import writing from "../data/writing.json";
 
 const WritingPage = () => (
-  <Container>
-    {Navbar(navPages)}
+  <Box>
     <Flex direction="column" gap="3">
       {writing.map(WritingElem)}
     </Flex>
-  </Container>
+  </Box>
 );
 
 export default WritingPage;
