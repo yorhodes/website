d44d984ecee25a02dd317ace2489f565720fcbc1
Yorke Rhodes <email@yorke.dev>
2024-08-21 15:06:03 -0400

    Make link search fuzzier

--- pages/api/[link].ts
+++ pages/api/[link].ts
@@ -1,16 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import links from "../../data/links.json";
+import talks from "../../data/talks.json";
 import writing from "../../data/writing.json";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const link: string = req.query.link?.toString() ?? "";
+  const whitespaceLink = link.replace(/-/g, " ");
 
-  const result = [...links, ...writing].find(
-    (o) =>
-      o.link.includes(link) ||
-      'label' in o && o.label?.includes(link) ||
-      'title' in o && o.title?.includes(link)
+  const findInValues = (object: Record<string, string | undefined>, search: string) =>
+    Object.entries<string | undefined>(object).find(([_, v]) => v?.toLowerCase().includes(search.toLowerCase()));
+
+  const result = [...links, ...writing, ...talks].find(
+    o => findInValues(o, link) || findInValues(o, whitespaceLink)
   );
 
   if (result) {
